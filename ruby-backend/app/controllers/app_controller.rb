be946176b6dd9ed904fccfd7d6a4234422cb882f
gatwi <faith.murithi@student.moringaschool.com>
2022-09-09 18:35:59 +0300

    updated app_controller

--- ruby-backend/app/controllers/app_controller.rb
+++ ruby-backend/app/controllers/app_controller.rb
@@ -25,27 +25,52 @@ class ApplicationController < Sinatra::Base
     #   comments.to_json
     # end
 
-    get "/comments" do
-        comments = Comment.all.order(:created_at)
-        comments.to_json
+    get "/users" do
+        users = User.all.order(:created_at)
+        users.to_json
     end
       
-      #sends info to the server
-      post "/comments" do
-        comment = Comment.create(comment: params[:comment], review: params[:review], rating: params[:rating], username: params[:username])
-        comment.to_json
+      
+    post "/users" do
+        user = User.create(first_name: params[:first_name], last_name: params[:last_name], username: params[:username], age: params[:age], email: params[:email], password: params[:password], location: params[:location])
+        user.to_json
     end
 
-      get "/comments" do
-        comments = Comment.all.order(:created_at)
-        comments.to_json
+    # patch "/commentss/:id" do
+    #   comments = comments.find(params[:id])
+    #   comments.update(body: params[:body])
+    #   comments.to_json
+    # end
+    
+    # delete "/commentss/:id" do
+    #   comments = comments.find(params[:id])
+    #   comments.destroy
+    #   comments.to_json
+    # end
+
+    get "/books" do
+        books = Book.all.order(:created_at)
+        books.to_json
     end
       
       #sends info to the server
-      post "/comments" do
-        comment = Comment.create(comment: params[:comment], review: params[:review], rating: params[:rating], username: params[:username])
-        comment.to_json
+    post "/books" do
+        book = Book.create(title: params[:title], author: params[:author], publication: params[:publication], genre: params[:genre])
+        book.to_json
     end
+
+    # patch "/commentss/:id" do
+    #   comments = comments.find(params[:id])
+    #   comments.update(body: params[:body])
+    #   comments.to_json
+    # end
+    
+    # delete "/commentss/:id" do
+    #   comments = comments.find(params[:id])
+    #   comments.destroy
+    #   comments.to_json
+    # end
+
     
 end
 
