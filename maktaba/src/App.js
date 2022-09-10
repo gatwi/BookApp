a834fec3094398d7d9018caafbf4ba092f5acf05
gatwi <faith.murithi@student.moringaschool.com>
2022-09-10 13:49:57 +0300

    routing update on my app

--- maktaba/src/App.js
+++ maktaba/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Catalog from "./pages/Catalog";
 import Library from "./pages/Library";
@@ -8,9 +9,19 @@ import Cart from "./pages/Cart";
 
 
 
+
 const App = () => {
   return (
-    <Catalog />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/catalog" element={<Catalog/>} />
+        <Route path="/lib" element={<Library/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/shopping" element={<Cart/>} />
+      </Routes>
+    </Router>
   );
 };
 
